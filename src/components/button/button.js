07e8eecda274de4ef923ecd965d34c22c73a6cb3
Matías Ezequiel Correa <matiasecorrea19@gmail.com>
2023-10-23 16:27:48 -0300

    Delete src/components directory

--- src/components/button/button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './button.css'
-
-const Button = ({ label }) => {
-    return <button className='btn' onClick={() => alert(`A label desse botão é: ${label}`)}>{label}</button>
-}
-
-export default Button
